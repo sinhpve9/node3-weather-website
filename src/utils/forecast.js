849e2a18fcec3b50ca652fa1176bb32b6750be97
Viet Sinh Pham <sinhpv656@gmail.com>
2022-12-07 12:35:36 +0700

    first commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,28 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    'http://api.weatherstack.com/current?access_key=238874efa094b05feb250e902732e7bd&query=' +
+    latitude +
+    ',' +
+    longitude +
+    '&units=m';
+
+  request({ url: url, json: true }, (error, response) => {
+    if (error) {
+      callback('Unable to connect to weather service!', undefined);
+    } else if (response.body.error) {
+      callback('Unable to find location', undefined);
+    } else {
+      callback(
+        undefined,
+        response.body.current.weather_descriptions[0] +
+          '. It is currently ' +
+          response.body.current.temperature +
+          ' degress out.'
+      );
+    }
+  });
+};
+
+module.exports = forecast;
